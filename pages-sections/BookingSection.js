32d7437863623ec87f8f9af32c60b0b8ff03946a
sseccombe <stevie.seccombe@kaluza.com>
2022-12-30 13:07:29 +0000

    main: updates

--- pages-sections/BookingSection.js
+++ pages-sections/BookingSection.js
@@ -13,13 +13,15 @@ export default function BookingSection() {
             <div style={{ textAlign: "center" }} className={classes.container}>
             <h2 className={classes.title}>MAKE A BOOKING</h2>
                 <div id="enquiries">
-                <div>To book email <a href="mailto:babette.lee1@gmail.com">babette.lee1@gmail.com</a> or call 07973 421048</div>
-                <p> </p>
-                        <div>Group workshops</div>
-                                <div>1-1 sessions</div>
-                                <div>Zoom sessions</div>
+                <div>Group workshops</div>
+                <div>1-1 sessions</div>
+                <div>Zoom sessions</div>
                                 <p> </p>
-                <div>Every 2nd Sunday of the month 10.30am - 12 noon - £15</div> 
+                <div>Beginners and all levels welcome</div>
+                <p> </p>
+                <div>To book or to make an enquiry email <a href="mailto:babette.lee1@gmail.com">babette.lee1@gmail.com</a> or call <a href="tel:07973 421048">07973 421048</a></div>         
+                <p> </p>
+                <div>Group workshops every 2nd Sunday of the month 10.30am - 12 noon - £15</div> 
                     <div>at</div> 
                     <div><a href="https://www.yogaandwellnessrooms.co.uk/">The Yoga and Wellness Rooms</a></div>
                 <div>The Old Town Hall, Town Quay, Shoreham-by-Sea BN43 5DD</div>
--- pages-sections/HomePageInfoSection.js
+++ pages-sections/HomePageInfoSection.js
@@ -5,6 +5,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import img from "assets/img/release.jpeg"
+import img2 from "assets/img/body.jpg"
 import styles from "assets/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -13,16 +14,15 @@ export default function HomePageInfoSection() {
     const classes = useStyles();
     return (
         <div id="info" className={classes.section}>
-            <GridContainer justify="center">
-            <img width="300px" alt="Release" src={img} />
-                <GridItem xs={12} sm={12} md={8}>
-                    <h4 className={classes.title}>TRE (TENSION/TRAUMA RELEASING EXERCISES)</h4>
+            <GridContainer justify="center" md={12}>
+            <GridItem xs={12} sm={12} md={4}><img alt="Release" src={img} width="240px" /></GridItem>
+                <GridItem xs={12} sm={12} md={4}>
+                    <h4 className={classes.title}>TENSION/TRAUMA RELEASING EXERCISES WITH BABETTE</h4>
                     <h5 className={classes.description}>
                     TRE (Tension/Trauma Releasing Exercises) are a set of 7 simple exercises which assist your body to safely activate a natural reflex mechanism of shaking/vibrating/movements. This helps to release deep-held stress/tension/trauma which signals to your mind & body that danger has passed and to turn off the flight/fight/freeze responses to past overwhelming events thereby resetting your system.</h5>
                     <h5 className={classes.description}>The aim of the sessions are to teach you the TRE process so that you are confident enough to be able to practice this self-healing tool yourself.</h5>
-                    <h5 className={classes.description}>Beginners and all levels welcome.
-                    </h5>
                 </GridItem>
+                <GridItem xs={12} sm={12} md={4}><img alt="Body" src={img2} width="280px" /></GridItem>
             </GridContainer>
         </div>
     );
