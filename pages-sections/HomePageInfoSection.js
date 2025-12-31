e1dca3cf06f8078f8188b990ebf6ebd71b8a265c
StevieSeccombe1 <slseccombe@googlemail.com>
2025-12-31 09:28:02 +0000

    Update HomePageInfoSection.js

--- pages-sections/HomePageInfoSection.js
+++ pages-sections/HomePageInfoSection.js
@@ -20,9 +20,10 @@ export default function HomePageInfoSection() {
                 <GridItem xs={12} sm={12} md={4}>
                     <h4 className={classes.title}>TENSION/TRAUMA RELEASING EXERCISES WITH BABETTE</h4>
                     <h5 className={classes.description}>
-                    TRE (Tension/Trauma Releasing Exercises) are a set of 7 simple exercises which assist your body to safely activate a natural reflex mechanism of shaking/vibrating/movements. This helps to release deep-held stress/tension/trauma which signals to your mind & body that danger has passed and to turn off the flight/fight/freeze responses to past overwhelming events thereby resetting your system.</h5>
-                    <h5 className={classes.description}>The aim of the sessions are to teach you the TRE process so that you are confident enough to be able to practice this self-healing tool yourself.</h5>
-                </GridItem>
+                    TRE (Tension/Trauma Releasing Exercises) consist of seven simple exercises that help your body safely activate a natural reflex of shaking/ vibrating/moving. This process releases deep-held stress, tension, and trauma, helping to turn off the fight/flight/freeze responses to past overwhelming events and resetting your system.</h5>
+                    <h5 className={classes.description}>The practice is simple, effective, and accessible to all ages, physical abilities and fitness levels.</h5>
+                    <h5 className={classes.description}>Once your body has remembered this innate self healing through TRE you then have a valuable lifelong tool for maintaining emotional and physical well-being to practice as and when you want to.</h5>
+                 </GridItem>
                 <GridItem xs={12} sm={12} md={4}><Image alt="Body" src={img2} objectFit="contain" objectPosition="top" /></GridItem>
             </GridContainer>
         </div>
